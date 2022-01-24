e5265966fab43fbc22ab998377d52acd2fd3215f
Allan Vestergaard <info@avz.dk>
2022-01-24 21:39:24 +0100

    Init

---
+++ activity.py
@@ -0,0 +1,25 @@
+import datetime
+
+class Activity():
+    def __init__(self,date,distance,time):
+        self.date=date
+        self.distance=distance
+        self.time = time
+
+    @property
+    def isoYW(self): #isoYear and isoWeek
+        return str()+f"{self.date.isocalendar()[0]}-{self.date.isocalendar()[1]:02}"
+
+    @property
+    def weekstart(self): #mandag i ugen
+        d=self.date
+        d+= datetime.timedelta(days=-self.date.weekday()) # mandag er 0
+        return d
+
+    def __str__(self):
+        return f"Activity: {self.date} ({self.isoYW}) {self.distance} km in {self.time}"
+
+
+if __name__ == "__main__":
+    my_activity=Activity(datetime.date.today(),42.2,242.5)
+    print(my_activity)
---
+++ traninghistory.py
@@ -0,0 +1,41 @@
+import csv
+import datetime
+from activity import Activity
+
+
+class Traininglog():
+
+    def __init__(self):
+        self.activities=[]
+
+    def _sortlist(self):
+        self.activities.sort(key=lambda x: x.date)
+
+    @property
+    def firstdate(self):
+        self.activities[0].date
+
+    @property
+    def lastdate(self):
+        self.activities[-1].date
+
+    
+    def readcsvfile(self,filename):
+        reader = csv.reader(open(filename), delimiter=";")
+        next(reader, None)  # skip the headers
+        for row in reader:
+            date=datetime.datetime.strptime(row[0], "%d/%m/%Y").date()
+            distance=float(row[1])
+            time=float(row[2][0:2])*60+float(row[2][3:5])+float(row[2][6:8])/60
+            print(time)
+            activity=Activity(date,distance,time)        
+            self.activities.append(activity)
+        self._sortlist()
+        return self.activities
+
+if __name__ == "__main__":
+    FILENAME="./testdata/5kmx3.csv"
+    traininglog=Traininglog()
+    traininglog.readcsvfile(FILENAME)
+    for i in traininglog.activities:
+        print(i)
