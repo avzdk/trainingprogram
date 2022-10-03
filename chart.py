048acf54759a6a746b3d90825800d12aa0579eaf
Allan Vestergaard <av@avz.dk>
2022-10-03 21:38:09 +0200

    Output til folder

--- chart.py
+++ chart.py
@@ -10,22 +10,22 @@ class plot():
     def drawByWeek(self,data):
         data['xlabel']=data["phase_number"].map(str) +data["phase_subphase"]
         fig1 = px.bar(data, y="xlabel", x="distance",  color="trainingtype", title=f"Running ({datetime.date.today()})", orientation='h', text_auto='.2s')
-        fig1.write_html(self.folder+'chartByWeek.html', auto_open=False)
+        fig1.write_html(self.folder+'/chartByWeek.html', auto_open=False)
 
     def drawByPhase(self,data):
         data['xlabel']=data["phase_number"].map(str)+":"+data["phase_startdate"].map(str)
         fig1 = px.bar(data, y="xlabel", x="distance",  color="trainingtype", title=f"Running ({datetime.date.today()})", orientation='h', text_auto='.2s')
-        fig1.write_html(self.folder+'chartByPhase.html', auto_open=False)
+        fig1.write_html(self.folder+'/chartByPhase.html', auto_open=False)
 
     def drawByDate(self,data):
         data['xlabel']=data["phase_number"].map(str) +data["date"].map(str)
         fig1 = px.bar(data, y="date", x="distance",  color="trainingtype", title=f"Running ({datetime.date.today()})", orientation='h', text_auto='.2s')
-        fig1.write_html(self.folder+'chartByDate.html', auto_open=False)
+        fig1.write_html(self.folder+'/chartByDate.html', auto_open=False)
 
     def drawSummary(self,data):
         print(data.columns)
         fig1 = px.line(data, y=['distance','maxdist'], x="phase_number",  title=f"Running ({datetime.date.today()})")
-        fig1.write_html(self.folder+'chartSummary.html', auto_open=False)
+        fig1.write_html(self.folder+'/chartSummary.html', auto_open=False)
 
 if __name__ == "__main__":
     None
